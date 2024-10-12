0b72a99b71f182b84fff96e685c1cdfec0e95e0e
Ibrahim Nasr <42195782+inasribrahim@users.noreply.github.com>
2024-10-12 04:36:20 +0300

    -accel off start_emu_headless.sh

--- start_emu_headless.sh
+++ start_emu_headless.sh
@@ -34,7 +34,7 @@ hw_accel_flag=$(check_hardware_acceleration)
 
 function launch_emulator () {
   adb devices | grep emulator | cut -f1 | xargs -I {} adb -s "{}" emu kill
-  options="@${emulator_name} -no-window -no-snapshot -screen no-touch -noaudio -memory 12000 -cores 12 -no-boot-anim ${hw_accel_flag} -camera-back none"
+  options="@${emulator_name} -no-window -no-snapshot -screen no-touch -noaudio -memory 12000 -cores 12 -no-boot-anim -accel off -camera-back none"
   if [[ "$OSTYPE" == *linux* ]]; then
     echo "${OSTYPE}: emulator ${options} -gpu off"
     nohup emulator $options -gpu off &
